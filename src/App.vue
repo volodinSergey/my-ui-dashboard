a1e3c6a4f7e391f21d4796edc302453da372ae92
Volodin Sergey <volodin7ergey@icloud.com>
2023-05-02 00:05:05 +0300

    [init:6-th step]: created main base  markdown(header, footer, navbar and main section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="init">
-    <h1 class="init__heading">I am init heading</h1>
+  <div class="app">
+    <header class="header">Header</header>
+
+    <main class="main">
+      <nav>Navigation with components</nav>
+
+      <section class="main">Pages will be here</section>
+    </main>
+
+    <footer class="footer">Footer</footer>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.app {
+  display: grid;
+  min-height: 100vh;
+  grid-template-rows: minmax(2.5rem, auto) 1fr minmax(2.5rem, auto);
+}
+
+.main {
+  display: grid;
+  grid-template-columns: minmax(150px, 20%) 1fr;
+}
+
+.header {
+  background-color: tomato;
+}
+
+.footer {
+  background-color: red;
+}
+</style>
