f71af9a10ad2209ad8f92527398e7c37e20ef609
Volodin Sergey <volodin7ergey@icloud.com>
2023-05-02 04:41:23 +0300

    [refactor: creating new layout]: created and used main layout

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,13 @@
 <script setup lang="ts">
-import Header from './widgets/Header.widget.vue'
-import Footer from './widgets/Footer.widget.vue'
+import MainLayout from './widgets/layouts/Main.layout.vue'
 </script>
 
 <template>
-  <div class="app">
-    <Header />
-
-    <main class="main">
-      <nav>Navigation with components</nav>
-
-      <router-view></router-view>
-    </main>
-
-    <Footer />
-  </div>
+  <MainLayout>
+    <template #router-content>
+      <router-view> </router-view>
+    </template>
+  </MainLayout>
 </template>
 
-<style scoped lang="scss">
-.app {
-  display: grid;
-  min-height: 100vh;
-  grid-template-rows: minmax(2.5rem, auto) 1fr minmax(2.5rem, auto);
-}
-
-.main {
-  display: grid;
-  grid-template-columns: minmax(150px, 20%) 1fr;
-}
-</style>
+<style scoped lang="scss"></style>
---
+++ src/widgets/layouts/Main.layout.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Header from '../Header.widget.vue'
+import Footer from '../Header.widget.vue'
+</script>
+
+<template>
+  <div class="main-layout">
+    <Header />
+
+    <main class="main">
+      <nav>Navigation with components</nav>
+
+      <slot name="router-content" />
+    </main>
+
+    <Footer />
+  </div>
+</template>
+
+<style scoped>
+.main-layout {
+  display: grid;
+  min-height: 100vh;
+  grid-template-rows: minmax(2.5rem, auto) 1fr minmax(2.5rem, auto);
+}
+
+.main {
+  display: grid;
+  grid-template-columns: minmax(150px, 20%) 1fr;
+}
+</style>
