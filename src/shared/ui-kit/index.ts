985a59289c998da24a567af6a8a7afef8899f766
Volodin Sergey <volodin7ergey@icloud.com>
2023-05-03 03:09:43 +0300

    [docs: useUIKit hook]: added docs for useUIKit hook

--- src/shared/ui-kit/index.ts
+++ src/shared/ui-kit/index.ts
@@ -7,4 +7,8 @@ const uiKit = {
     'UButton': UButton
 }
 
+/**
+ * @title useUIKit helper-hook
+ * @description Used for global registetring ui components to use them without import statement
+ */
 export const useUIKit = () => Object.entries(uiKit).forEach(item => app.component(item[0], item[1]))
